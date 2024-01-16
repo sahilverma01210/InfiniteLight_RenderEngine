a48ecab278660f90ddd2a499db7952fbdbc0e781
Sahil Verma <sahilverma0651@gmail.com>
2024-01-16 13:04:00 +0530

    Removed Vulkan from Core.

--- source/Engine/Core/HelloTriangle.h
+++ source/Engine/Core/HelloTriangle.h
@@ -2,51 +2,16 @@
 
 #include "stdafx.h"
 
-#if !VULKAN_API
-    #include "DXSampleHelper.h"
+#include "DXSampleHelper.h"
 
-    // Note that while ComPtr from Microsoft::WRL is used to manage the lifetime of resources on the CPU,
-    // it has no understanding of the lifetime of resources on the GPU. Apps must account
-    // for the GPU lifetime of resources to avoid destroying objects that may still be
-    // referenced by the GPU.
-    // An example of this can be found in the class method: OnDestroy().
-    using namespace Microsoft::WRL;
+// Note that while ComPtr from Microsoft::WRL is used to manage the lifetime of resources on the CPU,
+// it has no understanding of the lifetime of resources on the GPU. Apps must account
+// for the GPU lifetime of resources to avoid destroying objects that may still be
+// referenced by the GPU.
+// An example of this can be found in the class method: OnDestroy().
+using namespace Microsoft::WRL;
 
-    using namespace DirectX;
-#endif
-
-#if VULKAN_API
-const int MAX_FRAMES_IN_FLIGHT = 2;
-
-const std::vector<const char*> validationLayers = {
-    "VK_LAYER_KHRONOS_validation"
-};
-
-const std::vector<const char*> deviceExtensions = {
-    VK_KHR_SWAPCHAIN_EXTENSION_NAME
-};
-
-#ifdef NDEBUG
-const bool enableValidationLayers = false;
-#else
-const bool enableValidationLayers = true;
-#endif
-
-struct QueueFamilyIndices {
-    std::optional<UINT> graphicsFamily;
-    std::optional<UINT> presentFamily;
-
-    bool isComplete() {
-        return graphicsFamily.has_value() && presentFamily.has_value();
-    }
-};
-
-struct SwapChainSupportDetails {
-    VkSurfaceCapabilitiesKHR capabilities;
-    std::vector<VkSurfaceFormatKHR> formats;
-    std::vector<VkPresentModeKHR> presentModes;
-};
-#endif
+using namespace DirectX;
 
 class HelloTriangle
 {
@@ -68,41 +33,6 @@ private:
     UINT m_width;
     UINT m_height;
 
-#if VULKAN_API
-
-    UINT currentFrame = 0;
-
-    // Frame Buffer Dimentions
-    int m_fbWidth, m_fbHeight;
-
-    VkInstance instance;
-    VkDebugUtilsMessengerEXT debugMessenger;
-    VkSurfaceKHR surface;
-
-    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
-    VkDevice device;
-
-    VkQueue graphicsQueue;
-    VkQueue presentQueue;
-
-    VkSwapchainKHR swapChain;
-    std::vector<VkImage> swapChainImages;
-    VkFormat swapChainImageFormat;
-    VkExtent2D swapChainExtent;
-    std::vector<VkImageView> swapChainImageViews;
-    std::vector<VkFramebuffer> swapChainFramebuffers;
-
-    VkRenderPass renderPass;
-    VkPipelineLayout pipelineLayout;
-    VkPipeline graphicsPipeline;
-
-    VkCommandPool commandPool;
-    std::vector<VkCommandBuffer> commandBuffers;
-
-    std::vector<VkSemaphore> imageAvailableSemaphores;
-    std::vector<VkSemaphore> renderFinishedSemaphores;
-    std::vector<VkFence> inFlightFences;
-#else
     float m_aspectRatio;
 
     HWND m_hWnd;
@@ -144,63 +74,7 @@ private:
     HANDLE m_fenceEvent;
     ComPtr<ID3D12Fence> m_fence;
     UINT64 m_fenceValue;
-#endif
 
-#if VULKAN_API
-    void createInstance();
-
-    void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
-
-    void setupDebugMessenger();
-
-    void createSurface(HINSTANCE hInstance, HWND hWnd);
-
-    void pickPhysicalDevice();
-
-    void createLogicalDevice();
-
-    void createSwapChain();
-
-    void createImageViews();
-
-    void createRenderPass();
-
-    void createGraphicsPipeline();
-
-    void createFramebuffers();
-
-    void createCommandPool();
-
-    void createCommandBuffers();
-
-    void recordCommandBuffer(VkCommandBuffer commandBuffer, UINT imageIndex);
-
-    void createSyncObjects();
-
-    VkShaderModule createShaderModule(const std::vector<char>& code);
-
-    VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
-
-    VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
-
-    VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
-
-    SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
-
-    bool isDeviceSuitable(VkPhysicalDevice device);
-
-    bool checkDeviceExtensionSupport(VkPhysicalDevice device);
-
-    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
-
-    std::vector<const char*> getRequiredExtensions();
-
-    bool checkValidationLayerSupport();
-
-    static std::vector<char> readFile(const std::string& filename);
-
-    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData);
-#else
     std::wstring GetAssetFullPath(LPCWSTR assetName);
 
     void GetHardwareAdapter(
@@ -208,11 +82,8 @@ private:
         _Outptr_result_maybenull_ IDXGIAdapter1** ppAdapter,
         bool requestHighPerformanceAdapter = false);
 
-    void SetCustomWindowText(LPCWSTR text);
-
     void LoadPipeline();
     void LoadAssets();
     void PopulateCommandList();
     void WaitForPreviousFrame();
-#endif
 };
--- source/Engine/Core/stdafx.h
+++ source/Engine/Core/stdafx.h
@@ -10,32 +10,13 @@
 
 #include <windows.h>
 
-#define VULKAN_API false
-
 #include <shellapi.h>
 
-#if VULKAN_API // Vulkan App Includes
-	#define VK_USE_PLATFORM_WIN32_KHR
-	#include <vulkan/vulkan.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
+#include "d3dx12.h"
 
-	#include <iostream>
-	#include <fstream>
-	#include <stdexcept>
-	#include <algorithm>
-	#include <vector>
-	#include <cstring>
-	#include <cstdlib>
-	#include <cstdint>
-	#include <limits>
-	#include <optional>
-	#include <set>
-#else // DirectX App Includes
-	#include <d3d12.h>
-	#include <dxgi1_6.h>
-	#include <D3Dcompiler.h>
-	#include <DirectXMath.h>
-	#include "d3dx12.h"
-
-	#include <string>
-	#include <wrl.h>
-#endif
+#include <string>
+#include <wrl.h>
