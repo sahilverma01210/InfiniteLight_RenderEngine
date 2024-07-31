75256e805c4f8c47f4daafa35c9d6b6ca54627bb
Sahil Verma <sahilverma01210@gmail.com>
2024-07-31 18:22:02 +0530

    Miscellaneous changes.

--- Engine/Runtime/source/D3D12RHI.cpp
+++ Engine/Runtime/source/D3D12RHI.cpp
@@ -189,6 +189,21 @@ namespace Renderer::RHI
     {
         UINT dxgiFactoryFlags = 0;
 
+//#if defined(_DEBUG)
+//        // Enable the debug layer (requires the Graphics Tools "optional feature").
+//        // NOTE: Enabling the debug layer after device creation will invalidate the active device.
+//        {
+//            ComPtr<ID3D12Debug> debugController;
+//            if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
+//            {
+//                debugController->EnableDebugLayer();
+//
+//                // Enable additional debug layers.
+//                dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
+//            }
+//        }
+//#endif
+
         // DirectX Graphics Infrastructure (DXGI) - Core component of DirectX API.
         // Serves as an intermediary layer between Graphics APIs and the Graphics Hardware.
 
